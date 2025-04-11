933de12dd3cb6d0b50401fcd16a4d6c5116a6a97
Jesper Webel <jesperweble@outlook.com>
2025-04-11 09:18:03 +0200

    New file structure - made backend and frontend folders. - made .gitignore to not commit changes to the config.js within which I will be writing a secure code and username.

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports =
-{
-    database:
-    {
-        user: '',       // Enter your username here. Not put in for security
-        password: '',   // Enter your password here. Not put in for security
-        connectString: 'localhost:1521/OracleServiceFREE'
-    }
-};
