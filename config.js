12d55ec8dc9b840db81748545924853428cca327
Jesper Webel <jesperweble@outlook.com>
2025-04-11 08:11:15 +0200

    Initialization

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports =
+{
+    database:
+    {
+        user: '',       // Enter your username here. Not put in for security
+        password: '',   // Enter your password here. Not put in for security
+        connectString: 'localhost:1521/OracleServiceFREE'
+    }
+};
